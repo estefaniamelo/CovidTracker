ae571c451538ff225840603a2286cfacac26aa8b
estefaniamelo <melojoa@sheridancollege.ca>
2021-03-08 21:04:38 -0500

    Array to store selected province has been made and its been tested

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
 //global variable
 let go = {};
 
-var prname; //province name
-var date; //date 
-var numtotal; //total number of infections
-var numtoday; //number of infections for selected date
+var provinces = [];
 
 
 $(document).ready(main);
@@ -14,19 +11,18 @@ function main(){
 
     //loads JSON asynchronously
     let option = {
-        url: "http://ejd.songho.ca/ios/covid19_full.json",
+        url: "http://ejd.songho.ca/ios/covid19.json",
         type: "GET",
         dataType: "json"
     }; 
 
     $.ajax(option).then(data =>{
-        log("Success");
+        log("Successfully loaded JSON");
     
-        go.json= data;
+        go.json = data;
 
-        //selectedProvince("provinces");
 
-        }).catch( () => log("error to load JSON"));
+        }).catch( () => log("error loading JSON"));
 
     //$("option[value='canada']").prop("selected", true);
 }
@@ -35,9 +31,56 @@ function main(){
 document.getElementById("provinces").onchange = selectedProvince;
 
 function selectedProvince(prov){
-    // let provOptions = document.getElementById("provinces").addEventListener("click", selectedProvince());
-
     let provSelected = prov.target.value;
 
     log(provSelected);
+
+    let provData = [];
+
+    for(let i= 0; i < go.json.length; i++){
+        if(go.json[i].prname == provSelected){
+            provData.push(go.json[i]);
+        }
+    }
+
+    console.log(provData);
+
+    return provSelected;
 }
+
+
+
+
+
+// const ctx= document.getElementById('chart1').getContext('2d');
+
+// const chart = new Chart(ctx, {
+//     type: 'line',
+//     data: {
+//         labels: ['red', 'green', 'build'],
+//         datasets: [{
+//             label: '#of votes',
+//             data: [12, 19, 3],
+//             backgroundColor: [
+//                 'red',
+//                 'green',
+//                 'blue'
+//             ],
+//             borderColor: [
+//                 'red',
+//                 'green',
+//                 'blue'
+//             ],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero: true
+//                 }
+//             }]
+//         }
+//     }
+// });
