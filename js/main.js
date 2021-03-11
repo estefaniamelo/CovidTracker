caee4e4152708bee2bec10254fef47ba935eb415
estefaniamelo <melojoa@sheridancollege.ca>
2021-03-10 23:39:30 -0500

    Added function that creates two new arrays. One stores the values of daily reported cases and the other stores the dates as strings. These will be used later on to create charts in order to display the data.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
-//global variable
+//global variables
 let go = {};
-
-var provinces = [];
-
 
 $(document).ready(main);
 
@@ -21,7 +18,6 @@ function main(){
     
         go.json = data;
 
-
         }).catch( () => log("error loading JSON"));
 
     //$("option[value='canada']").prop("selected", true);
@@ -30,12 +26,14 @@ function main(){
 
 document.getElementById("provinces").onchange = selectedProvince;
 
+var provData = [];
+
 function selectedProvince(prov){
     let provSelected = prov.target.value;
 
     log(provSelected);
 
-    let provData = [];
+    provData = [];
 
     //here we create an array to store the dates for 
     //the province selected by the user
@@ -44,9 +42,7 @@ function selectedProvince(prov){
             provData.push(go.json[i]);
         }
     }
-
-    console.log(provData);
-
+ 
     //sorting the dates in the array using a compare function 
     provData.sort(function(a, b){
         if(a > b) return 1;
@@ -54,14 +50,16 @@ function selectedProvince(prov){
         return 0;
     });
 
-    let html = "";
-
+    valuesArr();
     console.log(provData);
 
     let provLength = provData.length;
+    console.log("this is the provLength " + provLength);
 
     let lastDate = provData[provLength-1];
+    console.log("this is the lastDate " + lastDate);
 
+    //the following 2 statements allow us to display the daily and total count of cases in the html 
     document.getElementById('dailycountnum').innerHTML =  lastDate.numtoday;
 
     document.getElementById('totalcountnum').innerHTML = lastDate.numtotal;
@@ -69,10 +67,60 @@ function selectedProvince(prov){
     return provSelected;
 }
 
+function valuesArr(){
+    
+    //calculates the ms 
+    const MS_PER_DAY = 24 * 60 * 60 * 1000; //ms
+
+    //values array stores all daily cases since 2020-01-31
+    let firstTime = new Date(provData[0].date).getTime();
+
+    let values = [];
+
+    for(let e of provData){
+
+        let currTime = new Date(e.date).getTime();
+
+        let index = (currTime-firstTime) / MS_PER_DAY;
+
+        if(values[index] === typeof(undefined)){
+            values[index] = 0;
+        } 
+
+        values[index] = e.numtoday;
+    }
+
+
+    //dates array stores all date strings since 2020-01-31
+    let firstDate = new Date(provData[0].date);
+
+    let dates = [];
+
+    for(let d of provData){
+        let currDate = new Date(d.date);
+
+        let dateIndex = (currDate - firstDate) / MS_PER_DAY;
+
+        dates[dateIndex] = d.date;
+    }
+
+    console.log("this is the values array " + values);
+
+    console.log("these are the dates " + dates);
+}
+
+
+// function updateValues(order){
+//     log(order);
+
+//     let html = "";
+
+//     go.json.forEach((e, i) => {
 
 
 
-
+//     })
+// }
 // const ctx= document.getElementById('chart1').getContext('2d');
 
 // const chart = new Chart(ctx, {
